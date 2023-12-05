5a67c93f4184b27749d1e737ed6d64e42797a5d1
abdumajidovkhushnud <abdumajidovkhushnud@gmail.com>
2023-12-05 13:18:47 +0100

    Create wechselgeld.py

---
+++ wechselgeld.py
@@ -0,0 +1,18 @@
+print ("zu Zahlen 16.83€")
+print ("gezahlt mit 50.00€")
+
+zuZahlen = 16.83
+gezahltMit = 50.00
+
+Wechselgeld = gezahltMit - zuZahlen
+
+print ()
+print (f"Sie bekommen {Wechselgeld:0.2f}€ zurück")
+print ()
+
+# E20 = int(Wechselgeld / 20)
+E20 = Wechselgeld // 20
+E10 = Wechselgeld // 10
+
+print(f"{E20:.0f} * 20€")
+print(f"{E10:.0f} * 10€")
